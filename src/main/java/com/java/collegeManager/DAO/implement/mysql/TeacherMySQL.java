9877b9acf6ad1ff81bc63cd4502ac700c0232f3e
37thirty-seven <glxming44yyds@gmail.com>
2025-07-01 15:30:03 +0800

    增加类依赖

--- src/main/java/com/java/collegeManager/DAO/implement/mysql/TeacherMySQL.java
+++ src/main/java/com/java/collegeManager/DAO/implement/mysql/TeacherMySQL.java
@@ -3,6 +3,8 @@ package com.java.collegeManager.DAO.implement.mysql;
 import com.java.collegeManager.DAO.TeacherDAO;
 import com.java.collegeManager.model.Teacher;
 import com.java.collegeManager.utils.DBConnectionUtil;
+import com.java.collegeManager.utils.ShowAlert;
+import javafx.scene.control.Alert;
 
 import java.sql.*;
 import java.time.LocalDate;
